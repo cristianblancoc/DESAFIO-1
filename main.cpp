a8c7306f1472e82430c4bd87877bc82a30bbdaf0
cristianblancoc <davidblanco17052002@gmail.com>
2025-09-22 17:59:29 -0500

    paso 1: creando el primer codigo que es para poder abrir el archivo de prueba para implementar el RLE para ver si funciona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include <fstream>
+# include<string>
 
 using namespace std;
 
 int main()
 {
-    cout << "MMM" << endl;
+     ofstream archivotex("salidaREL.txt");
+    if(!archivotex){
+         cerr <<"no se pudo abrir el archivo "<<endl;
+    }
+    archivotex<<"WWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWW "<<endl;
+
+
+
+    string archivo ="salidaREL.txt";
+    ifstream archivo1 (archivo);
+    if (!archivo1.is_open()) {
+        cerr << "No se pudo abrir el archivo: " << archivo << endl;
+        return 1;
+    }
+    cout <<"leyendo char por char"<<endl;
+    char c;
+    while (archivo1.get(c)){
+        cout <<c;
+    }
+    archivo1.close();
+
+
     return 0;
 }
