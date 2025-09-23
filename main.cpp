c84f65a6a3bdc4ab70ff0fbc49cf00b861b2d11c
cristianblancoc <davidblanco17052002@gmail.com>
2025-09-23 13:18:13 -0500

    metodo RLE

--- main.cpp
+++ main.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
 #include <fstream>
-# include<string>
+
 
 using namespace std;
 
 int main()
 {
-     ofstream archivotex("salidaREL.txt");
+  /*   ofstream archivotex("salidaREL.txt");
     if(!archivotex){
          cerr <<"no se pudo abrir el archivo "<<endl;
-    }
+    }x
     archivotex<<"WWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWW "<<endl;
 
 
-
-    string archivo ="salidaREL.txt";
+    */
+    char* archivo ="salidaREL.txt";
     ifstream archivo1 (archivo);
     if (!archivo1.is_open()) {
         cerr << "No se pudo abrir el archivo: " << archivo << endl;
         return 1;
     }
-    cout <<"leyendo char por char"<<endl;
+    cout << "Leyendo y comprimiendo con RLE..." << endl;
+
     char c;
-    while (archivo1.get(c)){
-        cout <<c;
+    char actual = '\0';
+    int conteo = 0;
+
+    while (archivo1.get(c)) {
+        if (conteo == 0) {
+            // Primer carácter
+            actual = c;
+            conteo = 1;
+        } else if (c == actual) {
+            // Mismo carácter, aumentar conteo
+            conteo++;
+        } else {
+            // Carácter distinto, imprimir resultado anterior
+            cout << conteo << actual;
+            // reiniciar para el nuevo carácter
+            actual = c;
+            conteo = 1;
+        }
     }
+
+    // al salir del while imprimir el último grupo
+    if (conteo > 0) {
+        cout << conteo << actual;
+    }
+
+    cout << endl;
+
     archivo1.close();
+
+
 
 
     return 0;
